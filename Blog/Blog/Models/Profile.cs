dcc4ede96ac3e10bbfc0a15a995677df278f1437
uamaxkh <uamaxkh@gmail.com>
2019-05-21 17:11:01 +0300

    FirstP

---
+++ Blog/Blog/Models/Profile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class Profile
+    {
+        public string Name { get; set; }
+        public string BirthDate { get; set; }
+        public string email { get; set; }
+        public string wayToFindBlog { get; set; }
+        public string Hobbies { get; set; }
+        public string favoriteDrink { get; set; }
+
+        public Profile(string Name, string BirthDate, string email,
+            string wayToFindBlog, string Hobbies, string favoriteDrink)
+        {
+            this.Name = Name;
+            this.BirthDate = BirthDate;
+            this.email = email;
+            this.wayToFindBlog = wayToFindBlog;
+            this.Hobbies = Hobbies;
+            this.favoriteDrink = favoriteDrink;
+        }
+
+        public Profile()
+        {
+
+        }
+    }
+}
---
+++ Blog/Blog/Models/Review.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class Review
+    {
+        public string AuthorName { get; set; }
+        public DateTime Date { get; set; }
+        public string Text { get; set; }
+    }
+}
