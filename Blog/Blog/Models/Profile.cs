e425ade9bf7a352102ccac8b40aa3a2b9fe31ce2
uamaxkh <uamaxkh@gmail.com>
2019-05-21 17:20:13 +0300

    DateUPD

--- Blog/Blog/Models/Profile.cs
+++ Blog/Blog/Models/Profile.cs
@@ -18,7 +18,7 @@ namespace Blog.Models
             string wayToFindBlog, string Hobbies, string favoriteDrink)
         {
             this.Name = Name;
-            this.BirthDate = BirthDate;
+            this.BirthDate = BirthDate.Substring(0, 4) + BirthDate.Substring(7, 3) + BirthDate.Substring(4, 3);
             this.email = email;
             this.wayToFindBlog = wayToFindBlog;
             this.Hobbies = Hobbies;
